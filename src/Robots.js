3056f1ef7a595a7eefe33acbc955b600d1e7d46e
Salim-Shaik <salim7868786@gmail.com>
2023-01-02 22:21:12 +0200

    cards done

--- src/Robots.js
+++ src/Robots.js
@@ -1,37 +1,33 @@
 export const robots = [
-{
-    id : 1,
-    name: 'Lea Gram',
-    username: 'Leas',
-    email : 'Leagram@gmail.com'
-},
-{
-    id : 2,
-    name: 'asddff',
-    username: 'wfrg',
-    email : 'eagram@gmail.com'
-},
-{
-    id : 3,
-    name: 'wdf',
-    username: 'sfsd',
-    email : 'Leagam@gmail.com'
-    
-},
+  {
+    id: 1,
+    name: "Lea Gram",
+    username: "Leas",
+    email: "Leagram@gmail.com",
+  },
+  {
+    id: 2,
+    name: "asddff",
+    username: "wfrg",
+    email: "eagram@gmail.com",
+  },
+  {
+    id: 3,
+    name: "wdf",
+    username: "sfsd",
+    email: "Leagam@gmail.com",
+  },
 
-{
-    id : 4,
-    name: 'wdf',
-    username: 'sfsd',
-    email : 'Leagam@gmail.com'
-    
-},
-{
-    id : 5,
-    name: 'wdf',
-    username: 'sfsd',
-    email : 'Leagam@gmail.com'
-    
-}
-
-]
+  {
+    id: 4,
+    name: "wdf",
+    username: "sfsd",
+    email: "Leagam@gmail.com",
+  },
+  {
+    id: 5,
+    name: "wdf",
+    username: "sfsd",
+    email: "Leagam@gmail.com",
+  },
+];
