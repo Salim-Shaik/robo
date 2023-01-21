77b34ccee47c3a77a0732a6dbe71a7082072d264
Salim-Shaik <salim7868786@gmail.com>
2023-01-21 12:24:22 +0200

    serach box designed. needs search properties

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const SearchBox  = () =>
+{return(
+    <div className = 'pa2'>
+        <input className = '_pa3 ba b--green bg-lightest-blue_'
+        type = 'search' 
+        placeholder='search robots'/>
+    </div>
+    );
+}
+
+export default SearchBox;
